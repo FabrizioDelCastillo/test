effd3ffeb00851fdb72c21f2c73a0176304c62cf
Fabrizio DC <136365365+FabrizioDelCastillo@users.noreply.github.com>
2023-10-21 15:04:11 -0500

    borre los registros de usuarios

---
+++ panel/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Usuarios(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30, null=False)
+    apellido = models.CharField(max_length=30, null=False)
+    correo = models.CharField(max_length=50, null=False)
+    telefono = models.CharField(max_length=20, null=False)
+    documento = models.CharField(max_length=20, null=False)
+    f_nac = models.DateField()
+    f_registro = models.DateTimeField(auto_now_add=True, null=True)
+    class Meta:  
+        db_table = 'usuarios' 
