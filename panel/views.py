a69e52aa4a3472dd1c864203c2ac31d0e23e8fc1
Fabrizio DC <136365365+FabrizioDelCastillo@users.noreply.github.com>
2023-10-23 14:40:32 -0500

    Agregué botones

--- panel/views.py
+++ panel/views.py
@@ -28,26 +28,56 @@ def agregar(request):
     else:
         return render(request, "crud_usuarios/agregar.html")
 
-def actualizar(request):
-    if request.method=='POST':
-         if request.POST.get('nombre') and request.POST.get('apellido') and request.POST.get('correo') and request.POST.get('telefono') and request.POST.get('documento') and request.POST.get('f_nac'):
-            user = Usuarios()
-            user.id = request.POST.get('id')
-            user.nombre = request.POST.get('nombre')
-            user.apellido = request.POST.get('apellido')
-            user.correo = request.POST.get('correo')
-            user.documento = request.POST.get('documento')
-            user.f_nac = request.POST.get('f_nac')  
-            user.f_registro         
-            user.save()            
-            return redirect('listar')      
-    else:
+def actualizar(request, idUsuario):
+    try: 
+        if request.method=='POST':
+            if request.POST.get('id') and request.POST.get('nombre') and request.POST.get('apellido') and request.POST.get('correo') and request.POST.get('telefono') and request.POST.get('documento') and request.POST.get('f_nac'):
+                user_id_old = request.POST.get('id')
+                user_old = Usuarios()
+                user_old = Usuarios.objects.get(id = user_id_old)
+                        
+                
+                user = Usuarios()
+                user.id = request.POST.get('id')
+                user.nombre = request.POST.get('nombre')
+                user.apellido = request.POST.get('apellido')
+                user.correo = request.POST.get('correo')
+                user.telefono = request.POST.get('telefono')
+                user.documento = request.POST.get('documento')
+                user.f_nac = request.POST.get('f_nac')  
+                user.f_registro = user_old.f_registro         
+                user.save()            
+                return redirect('listar')      
+        else:
+            users = Usuarios.objects.all()
+            user = Usuarios.objects.get(id=idUsuario)
+            datos = { 'usuarios' : users, 'usuario': user}
+            return render(request, "crud_usuarios/actualizar.html", datos)
+        
+    except Usuarios.DoesNotExist:
         users = Usuarios.objects.all()
-        datos = { 'usuarios' : users }
-        return render(request, "crud_usuarios/actualizar.html",datos)
-
-def eliminar(request):
+        user = None
+        datos = { 'usuarios' : users, 'usuario': user }
+        return render(request, "crud_usuarios/actualizar.html", datos)
     
+
+def eliminar(request, idUsuario):
+    try:
+        if request.method=='POST':
+            if request.POST.get('id'):
+                id_a_borrar = request.POST.get('id')
+                tupla = Usuarios.objects.get(id = id_a_borrar)
+                tupla.delete()
+                return redirect('listar')
+        else:        
+            users = Usuarios.objects.all()
+            user = Usuarios.objects.get(id = idUsuario)
+            datos = { 'usuarios' : users, 'usuario': user }
+            return render(request, "crud_usuarios/eliminar.html",datos)
+        
+    except Usuarios.DoesNotExist:
         users = Usuarios.objects.all()
-        datos = { 'usuarios' : users }
-        return render(request, "crud_usuarios/actualizar.html",datos)
+        user = None
+        datos =  { 'usuarios' : users, 'usuario' : user }
+        return render(request, "crud_usuarios/eliminar.html",datos)    
+    
